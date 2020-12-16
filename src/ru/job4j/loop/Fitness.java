449d22ea747a04b3472c8abd2b08eb2c1ebdd691
bilob <Bilobrovets@mail.ru>
2020-12-16 19:45:46 +0700

    5.4. Протеиновая диета [#417326]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -4,12 +4,11 @@ public class Fitness {
 
         public static int calc(int ivan, int nik) {
             int month = 0;
-            if (ivan < nik) {
-                month += 1;
-                while ((ivan *= 3) < (nik *= 2)) {
+                while (ivan < nik) {
+                    ivan *=3;
+                    nik *=2;
                     month += 1;
                 }
-            }
             return month;
         }
 }
