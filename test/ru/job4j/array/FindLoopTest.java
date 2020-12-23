1bffa68700c53b9300d5b0454b6d4b0d3ccd8d7c
Кирилл <Bilobrovets@mail.ru>
2020-12-23 22:48:51 +0700

    6.1. Классический поиск перебором. [#417235]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -26,8 +26,8 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas3ThenMinus1() {
-        int[] input = {5, 4, 7, 2, 1};
-        int value = 3;
+        int[] input = {5, 4, 3, 2, 1};
+        int value = 7;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
         assertThat(result, is(expect));
